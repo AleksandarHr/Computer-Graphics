3b0f48911132936b5883e24cdce82319e8998de2
Aleksandar Hrusanov <hrusanov@grinnell.edu>
2022-04-26 12:44:40 +0300

    Viewport

--- DrawSVG/src/viewport.cpp
+++ DrawSVG/src/viewport.cpp
@@ -12,7 +12,15 @@ void ViewportImp::set_viewbox( float centerX, float centerY, float vspan ) {
   this->centerX = centerX;
   this->centerY = centerY;
   this->vspan = vspan; 
+  Matrix3x3 translation = Matrix3x3::identity();
+  translation[2].x = vspan - centerX;
+  translation[2].y = vspan - centerY;
 
+  Matrix3x3 scaling = Matrix3x3::identity();
+  scaling[0].x = 1 / (2 * vspan);
+  scaling[1].y = 1 / (2 * vspan);
+
+  set_svg_2_norm(scaling * translation);
 }
 
 void ViewportImp::update_viewbox( float dx, float dy, float scale ) { 
